6510bb4f56f0b06e37789ccbcd7f49d77854582c
James La Guma <james@lsdev.biz>
2020-06-23 16:49:11 +0200

    URL change.

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ with open('README.md', encoding='utf8') as file:
 setup(
     name='hangman',
     version=hangman.__version__,
+    author=hangman.__author__,
+    author_email='james@taran.biz',
+    url='https://github.com/jlaguma/hangman',
     packages=['hangman'],
     package_data={},
     install_requires=[],
@@ -19,8 +22,5 @@ setup(
     description='Hangman Game',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author=hangman.__author__,
-    author_email='james@taran.biz',
-    url='https://www.linkedin.com/in/jlaguma/',
     entry_points={'console_scripts': ['hangman = hangman.hangman:hangman']},
 )
